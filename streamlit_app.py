a85d526069b53fd53d0317ebb6bad0ecd666e823
AJAY SUYAL <ajaysuyal48@gmail.com>
2025-05-17 17:34:45 +0530

    Update streamlit_app.py
    useing groq api

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,24 +1,24 @@
 import streamlit as st
-from openai import OpenAI
+from groq import GroqClient
 
 # Show title and description.
-st.title("💬 Chatbot")
+st.title("💬 Chatbot with Groq")
 st.write(
-    "This is a simple chatbot that uses OpenAI's GPT-3.5 model to generate responses. "
-    "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
+    "This is a simple chatbot that uses Groq's API to generate responses. "
+    "To use this app, you need to provide an Groq API key, which you can get [here](https://console.groq.com/docs/api-reference)). "
     "You can also learn how to build this app step by step by [following our tutorial](https://docs.streamlit.io/develop/tutorials/llms/build-conversational-apps)."
 )
 
 # Ask user for their OpenAI API key via `st.text_input`.
 # Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
 # via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
-openai_api_key = st.text_input("OpenAI API Key", type="password")
+groq_api_key = st.text_input("Groq API Key", type="password")
 if not openai_api_key:
-    st.info("Please add your OpenAI API key to continue.", icon="🗝️")
+    st.info("Please add your Groq API key to continue.", icon="🗝️")
 else:
 
-    # Create an OpenAI client.
-    client = OpenAI(api_key=openai_api_key)
+    # Create an Groq client.
+    client = GroqClient(api_key=Groq_api_key)
 
     # Create a session state variable to store the chat messages. This ensures that the
     # messages persist across reruns.
@@ -41,7 +41,7 @@ else:
 
         # Generate a response using the OpenAI API.
         stream = client.chat.completions.create(
-            model="gpt-3.5-turbo",
+            model="llama-3",
             messages=[
                 {"role": m["role"], "content": m["content"]}
                 for m in st.session_state.messages
@@ -52,5 +52,7 @@ else:
         # Stream the response to the chat using `st.write_stream`, then store it in 
         # session state.
         with st.chat_message("assistant"):
-            response = st.write_stream(stream)
-        st.session_state.messages.append({"role": "assistant", "content": response})
+            st.markdown(response["choices"][0]["message"]["content"])
+        st.session_state.messages.append({"role": "assistant", "content": response["choices"][0]["message"]["content"]})
+            #response = st.write_stream(stream)
+        #st.session_state.messages.append({"role": "assistant", "content": response})
