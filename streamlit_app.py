c993ba63c66fa015bd2eda87eee5438dac0a284e
AJAY SUYAL <ajaysuyal48@gmail.com>
2025-05-20 00:36:43 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,26 +1,16 @@
 import streamlit as st
-from groq import GroqClient
+from groq import Groq
+from dotenv import load_dotenv()
+
+load_dotenv()
+GROQ_API_KEY=st.getenv("GROQ_API_KEY")
 
 # Show title and description.
-st.title("💬 Chatbot with Groq")
+st.set_page_config(page_title="Chatbot", page_icon="🤖")
+st.title("🤖 Chatbot with Groq")
 st.write(
     "This is a simple chatbot that uses Groq's API to generate responses. "
-    "To use this app, you need to provide an Groq API key, which you can get [here](https://console.groq.com/docs/api-reference)). "
-    "You can also learn how to build this app step by step by [following our tutorial](https://docs.streamlit.io/develop/tutorials/llms/build-conversational-apps)."
 )
-
-# Ask user for their OpenAI API key via `st.text_input`.
-# Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
-# via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
-
-# ask For the API Key Securely
-groq_api_key = st.secrets["groq"]["api_key"]
-
-if not groq_api_key:
-    st.info("Please add your Groq API key to continue.", icon="🗝️")
-else:
-    # Create an Groq client.
-    client = GroqClient(api_key=Groq_api_key)
 
     # Create a session state variable to store the chat messages. This ensures that the
     # messages persist across reruns.
@@ -36,15 +26,27 @@ else:
     # automatically at the bottom of the page.
 
     # user input
-    if prompt := st.chat_input("What is up?"):
+    if prompt := st.chat_input("Type Here"):
         # Store and display the current prompt.
         st.session_state.messages.append({"role": "user", "content": prompt})
         with st.chat_message("user"):
             st.markdown(prompt)
 
+        '''headers={
+            "Authorization": f'Bearer {GROQ_API_KEY}",
+            "Content_type': 'application/json",
+        }
+            
+        body = {
+            'model'='llama3-8b-8192'
+            'message'=[
+                {'role':m['role'],'content':m['content']} 
+                for m in st.session_state.messages ]
+        }'''
+
         # Generate a response using the groq API.
         response = client.chat.completions.create(
-            model="llama-3",
+            model="llama3-70b-8192",
             messages=[
                 {"role": m["role"], "content": m["content"]}
                 for m in st.session_state.messages
@@ -55,7 +57,7 @@ else:
         # Stream the response to the chat using `st.write_stream`, then store it in 
         # session state.
         
-        # Diaplay assistant response
+        # Display assistant response
         with st.chat_message("assistant"):
             st.markdown(response["choices"][0]["message"]["content"])
         st.session_state.messages.append({"role": "assistant", "content": response["choices"][0]["message"]["content"]})
