0183986f997cb9cf54cd7bb0fa2eda60e7dc840b
AJAY SUYAL <ajaysuyal48@gmail.com>
2025-06-03 14:43:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import os
 
 # Load environment variables
 load_dotenv()
-GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+GROQ_API_KEY=os.getenv("GROQ_API_KEY")
 if GROQ_API_KEY is None:
     raise ValueError("groq_api_key is missing check Your File")
 
